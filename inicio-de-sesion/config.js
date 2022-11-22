3bcb94cf0b5ee6233551ef1ce49974703b01310f
rodrigosoberon <rodrigo.soberon.a@gmail.com>
2022-11-22 17:22:59 -0300

    inicio-de-sesion

---
+++ inicio-de-sesion/config.js
@@ -0,0 +1,6 @@
+const mongoRemote = {
+        client: 'mongodb',
+        cnxStr: 'mongodb+srv://rsoberon:contigo$1mac@cluster0.dola79s.mongodb.net/ecommerce?retryWrites=true&w=majority'
+    }
+
+module.exports = {mongoRemote}
---
+++ inicio-de-sesion/server.js
@@ -0,0 +1,71 @@
+const express = require('express')
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const config = require('./config');
+const advancedOptions = {useNewUrlParser: true, useUnifiedTopology: true}
+const hbs = require('express-handlebars');
+const passport = require('passport')
+const { initPassport } = require('./middlewares/passport.js')
+const authWebRouter = require("./routers/web/auth.js");
+const homeWebRouter = require("./routers/web/home.js");
+
+// ---------------------------------------------------------------------------------
+// CONFIGURACION SERVIDOR
+
+const app = express();
+const PORT = 8080
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(express.static('public'))
+initPassport()
+
+// ----------------------------------------------------------------------------------
+// MANEJO DE SESSION
+
+app.use(session({
+    store: MongoStore.create({
+        mongoUrl: config.mongoRemote.cnxStr,
+        mongoOptions: advancedOptions
+    }),
+    secret: 'shhhhhhhhhhhhhhhhhhhhh',
+    resave: false,
+    saveUninitialized: false,
+    rolling: true,
+    cookie: {
+        maxAge: 60000
+    }
+}))
+
+initPassport()
+app.use(passport.initialize())
+app.use(passport.session())
+
+// ----------------------------------------------------------------------------------
+// RUTAS
+
+app.use(authWebRouter)
+app.use(homeWebRouter)
+
+// ----------------------------------------------------------------------------------
+// SETUP MOTOR PLANTILLAS (HANDLEBARS)
+
+app.engine(
+    "hbs",
+    hbs.engine({
+        extname: ".hbs",
+        partialDir: "./views/partials",
+        layoutDir: "./views/layouts",
+        defaultLayout: "layout.hbs",
+    })
+);
+app.set("views", "./views");
+app.set("view engine", "hbs");
+
+
+// ----------------------------------------------------------------------------------
+// INICIO DEL SERVER
+
+const server = app.listen(PORT, () => {
+    console.log(`Servidor escuchando en puerto ${server.address().port}`);
+});
+server.on("error", (error) => console.log(`Error en servidor: ${error}`));
