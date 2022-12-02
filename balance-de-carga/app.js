399c2e7883a7c786d03a27a455d3614800bf655c
rodrigosoberon <rodrigo.soberon.a@gmail.com>
2022-12-02 18:54:49 -0300

    balance-de-carga

---
+++ balance-de-carga/app.js
@@ -0,0 +1,49 @@
+const cluster = require('cluster')
+const numCPUs = require('os').cpus().length
+const initServer = require('./server')
+const yargs = require('yargs/yargs')(process.argv.slice(2))
+
+const app = initServer()
+
+// ---------------------------------------------------------------------------------
+//ARGUMENTOS
+
+const args = yargs.default({
+    p: 8080,
+    m: 'FORK'
+}).argv
+const PORT = args.p
+//const PORT = process.argv[2] || 8080
+const MODE = args.m
+
+// ---------------------------------------------------------------------------------
+//EJECUTAR SERVIDOR
+
+function runServer(){
+    try{
+        app.listen(PORT)
+        console.log(`Worker ${process.pid} started`)
+    } catch (err){
+        console.log(err)
+    }
+}
+
+
+// ---------------------------------------------------------------------------------
+//SELECCION DE MODO (FORK O CLUSTER)
+
+if(MODE === 'CLUSTER'){
+    if(cluster.isPrimary){
+        console.log(`Master ${process.pid}`)
+        for(let i=0; i < numCPUs; i++){
+            cluster.fork()
+        }
+        cluster.on('exit', (worker, coder, sinal)=>{
+            console.log(`Worker ${worker.process.pid} died`)
+        })
+    }else{
+        runServer()
+    }
+}else{
+    runServer()
+}
---
+++ balance-de-carga/server.js
@@ -0,0 +1,97 @@
+const express = require('express')
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const config = require('./config');
+const hbs = require('express-handlebars');
+const passport = require('passport')
+const {initPassport} = require('./middlewares/passport.js')
+const authWebRouter = require("./routers/web/auth.js");
+const homeWebRouter = require("./routers/web/home.js");
+const infoWebRouter = require("./routers/web/info.js");
+const randomsWebRouter = require("./routers/web/randoms.js")
+
+
+
+
+const initServer = () => {
+
+// ---------------------------------------------------------------------------------
+// CONFIGURACION SERVIDOR
+
+
+
+    const app = express();
+    app.use(express.json())
+    app.use(express.urlencoded({extended: true}))
+    app.use(express.static('public'))
+    initPassport()
+
+// ----------------------------------------------------------------------------------
+// MANEJO DE SESSION
+    const advancedOptions = {useNewUrlParser: true, useUnifiedTopology: true}
+
+    app.use(session({
+        store: MongoStore.create({
+            mongoUrl: config.mongoRemote.cnxStr,
+            mongoOptions: advancedOptions
+        }),
+        secret: 'shhhhhhhhhhhhhhhhhhhhh',
+        resave: false,
+        saveUninitialized: false,
+        rolling: true,
+        cookie: {
+            maxAge: 60000
+        }
+    }))
+
+    initPassport()
+    app.use(passport.initialize())
+    app.use(passport.session())
+
+// ----------------------------------------------------------------------------------
+// RUTAS
+
+    app.use(authWebRouter)
+    app.use(homeWebRouter)
+    app.use(infoWebRouter)
+    app.use(randomsWebRouter)
+
+    app.get('/datos', (req, res)=>{
+        console.log(`puerto ${PORT}`)
+        res.send(`Servidor express - PORT ${PORT} - PID : ${pid} - FyH: ${new Date().toLocaleString()}`)
+    })
+
+
+
+// ----------------------------------------------------------------------------------
+// SETUP MOTOR PLANTILLAS (HANDLEBARS)
+
+    app.engine(
+        "hbs",
+        hbs.engine({
+            extname: ".hbs",
+            partialDir: "./views/partials",
+            layoutDir: "./views/layouts",
+            defaultLayout: "layout.hbs",
+        })
+    );
+    app.set("views", "./views");
+    app.set("view engine", "hbs");
+
+
+// ----------------------------------------------------------------------------------
+// INICIO DEL SERVER
+
+    return{
+        listen: port => new Promise((res,rej)=>{
+            const server = app.listen(port, () => {
+                console.log(`Servidor escuchando en puerto ${server.address().port}`);
+            });
+            server.on("error", (error) => console.log(`Error en servidor: ${error}`));
+        })
+    }
+
+
+}
+
+module.exports = initServer
